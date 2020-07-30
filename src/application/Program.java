7b9d2d67b0fb3a48617671724fe2db3a63e302f8
Leandro Klinger <Leandro.klingeroliveira@gmail.com>
2020-07-30 20:59:50 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -19,34 +18,35 @@ public class Program {
 		System.out.println("=== TEST 1: Seller findById ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n=== TEST 2: Seller findByDepartment ====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
-		for(Seller obj:list) {
+
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-		System.out.println("\n=== TEST 3: Seller findall ====");		
-		
+
+		System.out.println("\n=== TEST 3: Seller findall ====");
+
 		list = sellerDao.findAll();
-		
-		for(Seller obj:list) {
+
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n=== TEST 4: Seller insert ====");
-		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		Seller newSeller = new Seller(
-				null,"Lydia","lydia@gmail.com",sdf.parse("07/08/1995"),2000.0,department);
-		
+		Seller newSeller = new Seller(null, "Lydia", "lydia@gmail.com", sdf.parse("07/08/1995"), 2000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("INserted! New id = " + newSeller.getId());
+
 		
-		
+		System.out.println("\n=== TEST 5: Seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("KLINGER OLIVEIRA");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
 	}
 
 }
