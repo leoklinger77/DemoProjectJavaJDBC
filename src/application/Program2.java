b228530fd94ba5b072fc1a7e363471d0b5ec0f19
Leandro Klinger <Leandro.klingeroliveira@gmail.com>
2020-07-30 22:00:17 -0300

    Delete Department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,18 +11,28 @@ public class Program2 {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		DepartmentDao departmentDao = DaoFactory.createDeparmentDao();
 
-		System.out.println("=== TEST 1: Seller findById ====");
+		System.out.println("=== TEST 1: Department findById ====");
 		Department dep = departmentDao.findById(2);
 		System.out.println(dep);
 
-		System.out.println("\n=== TEST 2: Seller findAll ====");
+		System.out.println("\n=== TEST 2: Department findAll ====");
 		List<Department> list = departmentDao.findAll();
 		for (Department obj : list) {
-			System.out.println(obj);
+			System.out.println(obj);		
+			
 		}
+		
+		System.out.println("\n=== TEST 3: Department delete ====");
+		System.out.print("\nEnter id of delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete Completed");
 
+		
+		sc.close();
 	}
 
 }
